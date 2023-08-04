3d628271249162a827639701d8564e030d95e8b9
Muhammad Saqib <m.sxqib@gmail.com>
2023-08-04 23:11:26 +0500

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -28,24 +28,44 @@ class OpenAITranslate:
     def __str__(self):
         return "OpenAI"
     
-async def openai_translate(text, source_language, target_language):
-    prompt = f"""You are now an advanced translator, your role is to provide translations that mirror the fluency and subtleties of a native speaker. You have the capability to handle a wide range of languages, You can also accept unique and entertaining translation styles to translate into which are provided by the user, such as UwU. Your responses should be strictly confined to the translated text, without any additional or extraneous content.
-
-Your task is to translate the following text: "{text}", from the source language: "{source_language}", into the target language: "{target_language}". Type your translation here: """
-    max_tokens = 3200
-    encoding = tiktoken.encoding_for_model("text-davinci-003")
+async def openai_translate(text, source_language, target_language, model):
+    prompt = "You are now an advanced translator, your role is to provide translations that mirror the fluency and subtleties of a native speaker. You have the capability to handle a wide range of languages, You can also accept unique and entertaining translation styles to translate into which are provided by the user, such as UwU. Your responses should be strictly confined to the translated text, without any additional or extraneous content."
+    if model == "gpt-4":
+        max_tokens = 6000
+        max_limit = 8192
+    else:
+        max_tokens = 3000
+        max_limit = 4097
+    encoding = tiktoken.encoding_for_model(model)
     num_tokens = len(encoding.encode(prompt))
-    if (num_tokens + max_tokens) > 4097:
+    if (num_tokens + max_tokens) > max_limit:
         raise ValueError(
             "Your text is too long! "
         )
-    response = await openai.Completion.acreate(
-        model="text-davinci-003",
-        prompt=prompt,
+    try:
+        response = openai.ChatCompletion.acreate(
+        model=model,
         max_tokens=max_tokens,
         temperature=0,
-    )
-    return response['choices'][0]['text'].strip()
+        messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": f"Translate the following text: '{text}', from the source language: '{source_language}', into the target language: '{target_language}'"}
+            ]
+        )
+        translation = response['choices'][0]['message']['content']
+    except:
+        response = openai.ChatCompletion.acreate(
+        model="gpt-3.5-turbo",
+        max_tokens=max_tokens,
+        temperature=0,
+        messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": f"Translate the following text: '{text}', from the source language: '{source_language}', into the target language: '{target_language}'"}
+            ]
+        )
+        translation = response['choices'][0]['message']['content']
+    
+    return translation
 
 bot = discord.Bot()
 
@@ -140,20 +160,31 @@ async def translate(ctx, text: str = None, from_lang: str = None, to_lang: str =
     try:
         if isinstance(translator, OpenAITranslate):
             try:
-                translated_text = await openai_translate(text, from_lang, to_lang)
-            except Exception:
+                translated_text = await openai_translate(text, from_lang, to_lang, "gpt-4")
+            except Exception as E:
+                if "Your text is too long!" in str(E):
+                    embed4 = discord.Embed(
+                        title="❌ An error occured!",
+                        description="Your text is too long to translate!",
+                        color=discord.Colour.red(),
+                    )
+                    embed4.set_footer(text="Made by TranslatorBot team.")
+
+                    await ctx.respond(embed=embed4, ephemeral=True)
+                    return
+                else:
                 # Fallback to DeeplTranslate telling that it has been fallbacked
-                embed1 = discord.Embed(
-                    title="Fallbacked to DeeplTranslate!",
-                    description="An error occured while translating the text using OpenAI, translation will be done using Deepl",
-                    color=discord.Colour.orange(),
-                )
-                embed1.set_footer(text="Made by TranslatorBot team.")
+                    embed1 = discord.Embed(
+                        title="Fallbacked to DeeplTranslate!",
+                        description="An error occured while translating the text using OpenAI, translation will be done using Deepl",
+                        color=discord.Colour.orange(),
+                    )
+                    embed1.set_footer(text="Made by TranslatorBot team.")
 
-                await ctx.respond(embed=embed1, ephemeral=True)
+                    await ctx.respond(embed=embed1, ephemeral=True)
 
-                translator = DeeplTranslate()
-                translated_text = await translatefunc(loop, text, from_lang, to_lang, translator)
+                    translator = DeeplTranslate()
+                    translated_text = await translatefunc(loop, text, from_lang, to_lang, translator)
         else:
             translated_text = await translatefunc(loop, text, from_lang, to_lang, translator)
     except Exception as E:
