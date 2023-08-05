7e62f115c39e904716dfbf02bf66a3d3e3c55004
Muhammad Saqib <m.sxqib@gmail.com>
2023-08-05 16:02:58 +0500

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -128,7 +128,8 @@ async def bot_status():
         else:
             loop = asyncio.get_event_loop()
             try:
-                translated_text = await translatefunc(loop, text, from_lang, to_lang, translator)
+                translator_obj = await fetch_translator(translator)
+                translated_text = await translatefunc(loop, text, from_lang, to_lang, translator_obj)
                 if len(translated_text) > 0:
                     status[translator] = "🟢 Working, currently available."
                 else:
